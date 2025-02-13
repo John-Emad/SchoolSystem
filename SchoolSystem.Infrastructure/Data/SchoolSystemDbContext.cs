d698fda73cd312849a51affb5983cc53f25b83af
John-Emad <john_emad7@outlook.com>
2025-02-13 14:47:08 +0200

    Add project files.

---
+++ SchoolSystem.Infrastructure/Data/SchoolSystemDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolSystem.Domain;
+
+
+namespace SchoolSystem.Infrastructure.Data
+{
+    public class SchoolSystemDbContext : DbContext
+    {
+        public SchoolSystemDbContext()
+        {
+            
+        }
+
+        public SchoolSystemDbContext(DbContextOptions<SchoolSystemDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<DepartmentSubject> DepartmentSubjects { get; set; }
+        public DbSet<StudentSubject> StudentSubjects { get; set; }
+    }
+}
